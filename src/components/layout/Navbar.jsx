53306022c9b5fd02bbfe99577ce2c21c81dda99c
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-02-28 12:58:01 +0100

    Added individual branches

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,18 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
 
 export class Navbar extends Component {
-  static propTypes = {
-
-  }
+  static propTypes = {};
 
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div />;
   }
 }
 
-export default Navbar
+export default Navbar;
