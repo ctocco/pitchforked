5270ef4e7d5f3db6107819d99d21898d4ec4c2ad
julian0becker <julianentwickelt@gmail.com>
2019-02-28 14:07:33 +0100

    simple spotify fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import queryString from "querystring";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    artists: [],
+    turnOn: false
+  };
+
+  handleSearchArtist = event => {
+    event.preventDefault();
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed["?access_token"];
+    fetch(
+      `https://api.spotify.com/v1/search?q=${
+        event.target.searchArtist.value
+      }&type=artist`,
+      {
+        headers: { Authorization: "Bearer " + accessToken }
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        return this.setState({
+          artists: data.artists.items,
+          turnOn: true
+        });
+      });
+  };
+
+  componentDidMount() {}
+
   render() {
+    const artists = [...this.state.artists];
+    artists.splice(5);
     return (
       <div className="App">
-       Hello World
+        <h2>Hello World</h2>
+        {true ? (
+          <button
+            onClick={() => (window.location = "http://localhost:8888/login")}
+          >
+            Sign in with Spotify
+          </button>
+        ) : (
+          <p>signed in</p>
+        )}
+        <form onSubmit={event => this.handleSearchArtist(event)}>
+          <input type="text" name="searchArtist" />
+          <input type="submit" value="search" />
+          {this.state.turnOn ? (
+            artists.map(artist => <Display artist={artist} />)
+          ) : (
+            <p>Sign in and click search</p>
+          )}
+        </form>
+        <p />
       </div>
     );
   }
 }
 
 export default App;
+
+const Display = props => (
+  <div>
+    <h2>{props.artist.name}</h2>
+  </div>
+);
