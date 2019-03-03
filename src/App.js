bfde8d917162658ab17cedc7f95f72b0fde63792
julian0becker <julianentwickelt@gmail.com>
2019-03-03 14:02:02 +0100

    artist page mechanics set up

--- src/App.js
+++ src/App.js
@@ -5,37 +5,42 @@ import "./App.css";
 class App extends Component {
   state = {
     artists: [],
-    turnOn: false
+    turnOn: false,
+    selectedArtist: null,
+    artistPageOn: false
   };
 
-  handleSearchArtist = event => {
+  handleSearchArtist = async event => {
     event.preventDefault();
+    event.persist();
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed["?access_token"];
-    fetch(
-      `https://api.spotify.com/v1/search?q=${
-        event.target.searchArtist.value
-      }&type=artist`,
-      {
-        headers: { Authorization: "Bearer " + accessToken }
-      }
-    )
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        return this.setState({
-          artists: data.artists.items,
-          turnOn: true
-        });
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${
+          event.target.searchArtist.value
+        }&type=artist`,
+        {
+          headers: { Authorization: "Bearer " + accessToken }
+        }
+      );
+      const data = await response.json();
+      this.setState({
+        artists: data.artists.items,
+        turnOn: true
       });
+    } catch (error) {
+      console.log(error);
+    }
+
     event.target.searchArtist.value = "";
   };
 
-  componentDidMount() {}
+  handleLoadArtistPage = name => {
+    this.setState({ selectedArtist: name, artistPageOn: true });
+  };
 
   render() {
-    const artists = [...this.state.artists];
-    artists.splice(5);
     return (
       <div className="App">
         <h2>Hello World</h2>
@@ -54,12 +59,14 @@ class App extends Component {
         <form onSubmit={event => this.handleSearchArtist(event)}>
           <input type="text" name="searchArtist" />
           <input type="submit" value="search" />
-          {this.state.turnOn ? (
-            artists.map(artist => <Display artist={artist} />)
-          ) : (
-            <p>Sign in and click search</p>
-          )}
         </form>
+        <Display
+          turnOn={this.state.turnOn}
+          artists={this.state.artists}
+          handleLoadArtistPage={this.handleLoadArtistPage}
+          selectedArtist={this.state.selectedArtist}
+          artistPageOn={this.state.artistPageOn}
+        />
         <p />
       </div>
     );
@@ -69,20 +76,54 @@ class App extends Component {
 export default App;
 
 const Display = props => {
+  const artists = [...props.artists];
+  const filteredArtist = artists.filter(
+    artist => artist.name === props.selectedArtist
+  );
+  artists.splice(5);
+  return (
+    <div>
+      {props.artistPageOn ? (
+        <ArtistInfo artist={filteredArtist} />
+      ) : props.turnOn ? (
+        artists.map(artist => (
+          <Artist
+            handleLoadArtistPage={props.handleLoadArtistPage}
+            artist={artist}
+          />
+        ))
+      ) : (
+        <p>Sign in and click search</p>
+      )}
+    </div>
+  );
+};
+
+const Artist = props => {
   return (
     <div>
       <h2>{props.artist.name}</h2>
-      {props.artist.images.map(image => (
-        <img alt={props.artist.name} src={image.url} />
-      ))}
-      <p>
+      {props.artist.images.length === 0 ? null : (
+        <img
+          onClick={() => props.handleLoadArtistPage(props.artist.name)}
+          src={props.artist.images[1].url}
+          alt={props.artist.name}
+        />
+      )}
+      <div>
         {props.artist.genres.map(genre => (
           <div>
             {genre}
             <br />
           </div>
         ))}
-      </p>
+      </div>
     </div>
+  );
+};
+
+const ArtistInfo = props => {
+  return (
+    <div>{`this is going to be ${props.artist[0].name}'s artist page. `}</div>
   );
 };
