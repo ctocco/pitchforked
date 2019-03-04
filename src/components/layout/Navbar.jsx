87aee7db9b354a35f177d64dc891b176fde3d1cc
Michael Scully <42539704+MScully88@users.noreply.github.com>
2019-03-04 12:21:38 +0100

    Add files via upload
    Reactstrap Navbar & footer components

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,43 @@
-import React, { Component } from "react";
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-export class Navbar extends Component {
-  static propTypes = {};
 
+export default class MainNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
-    return <div />;
+    return (
+      <div>
+        <Navbar color="faded" className="navbar-inner" light>
+        <NavbarBrand href="/" className="mr-auto"></NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="./pages/LatestNews">Latest News</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="./pages/LatestMusic">Latest Music</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="./pages/UpcomingGigs">Upcoming Gigs</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
-
-export default Navbar;
