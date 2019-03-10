17ecb3040a674fe0c2b2bfdec37fac3a93feb2f9
Michael Scully <42539704+MScully88@users.noreply.github.com>
2019-03-10 17:11:17 +0100

    Search button & footer updated
     w/ ScrollableAnchors, styling and formatting

--- src/components/layout/SearchBar.jsx
+++ src/components/layout/SearchBar.jsx
@@ -1,20 +1,35 @@
 import React from "react";
-import { animateScroll as scroll } from "react-scroll";
-import { Input, Button } from 'reactstrap';
+import { Input, Button } from "reactstrap";
+import { goToAnchor } from 'react-scrollable-anchor';
 
 const SearchBar = props => {
   return (
     <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <h1 className="page-title">PITCHFORK'D</h1>
-      <form className="d-flex justify-content-center" onSubmit={event => props.handleSearchArtist(event)}>
-        <Input className="search-input" type="text" name="searchArtist"  size="default" />
+      <form
+        className="d-flex justify-content-center"
+        onSubmit={event => props.handleSearchArtist(event)}
+      >
+        <Input
+          className="search-input"
+          type="text"
+          name="searchArtist"
+          size="default"
+        />
         <Button
           outline
           className="search-button btn btn-dark"
           size="sm"
           type="submit"
-          onClick={() => scroll.scrollTo(987) }
-        >Search</Button>
+          onClick={(goToAnchor('#lower-page-scroll', true))}
+        >
+         Search
+        </Button>
       </form>
     </div>
   );
