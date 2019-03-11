3412967f651e071a6d9c6aaffea4827fcddc1d99
Marcel R <m-ruckpaul@t-online.de>
2019-03-11 13:55:59 +0100

    about changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,19 +15,19 @@ const About = () => {
                 <ul>
                     <li>
                         <h4 className="center abouttitle2"><b>Search</b> your artists</h4><br/>
-                            <p className="text-justify media abouttxt"><img className="aboutimg" src={Microphones} alt="About-1"/>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae!</p>
+                            <p className="text-justify media abouttxt"><img className="aboutimg" src={Microphones} alt="About-1"/>Search your favourite artist and refresh your love for their work with Pitchfork'd!</p>
                     </li><br/>
                     <li>
                         <h4 className="center abouttitle2"><b>Find</b> new artists</h4><br/>
-                            <p className="text-justify media abouttxt right"><img className="aboutimg" src={Mglass} alt="About-1"/>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae!</p>
+                            <p className="text-justify media abouttxt">Pitchfork'd makes it easy searching for new Artists too, for example we are suggesting similar artists when searching!<img className="aboutimg" src={Mglass} alt="About-1"/></p>
                     </li><br/>
                     <li>
                         <h4 className="center abouttitle2"><b>Explore</b> new events</h4><br/>
-                            <p className="text-justify media abouttxt"><img className="aboutimg" src={Calender} alt="About-1"/>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae!</p>
+                            <p className="text-justify media abouttxt"><img className="aboutimg" src={Calender} alt="About-1"/>Pitchfork'd is helping you finding all events in your area for your favourite artists too!</p>
                     </li><br/>
                     <li>
                         <h4 className="center abouttitle2"><b>Listen</b> to music</h4><br/>
-                            <p className="text-justify media abouttxt right"><img className="aboutimg" src={Headphones} alt="About-1"/>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt odit accusantium quidem perferendis repellat veritatis cupiditate et, ullam nulla beatae!</p>
+                            <p className="text-justify media abouttxt">Pitchfork'd also allows you to listen to the newest songs out there!<img className="aboutimg" src={Headphones} alt="About-1"/></p>
                     </li><br/>
                 </ul>
                 <br/>
