223556ce3ef5807e7bca098a48cc8cb23e881bf0
Marcel R <m-ruckpaul@t-online.de>
2019-03-13 14:53:40 +0100

    Update DispRecommendations.jsx

--- src/components/Content/DispRecommendations.jsx
+++ src/components/Content/DispRecommendations.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import "./css/DispRecommendations.css";
+import './css/DispRecommendations.css';
+
 
 const Recommendations = props => {
-  const recommend = !!props.artistRecommendations ? (
-    props.artistRecommendations.artists.map(recommend => {
-      return (
-        <ul className="disp-container">
-          <li>{recommend.name}</li>
-        </ul>
-      );
-    })
-  ) : (
-    <p>Loading...</p>
-  );
 
-  return (
-    <div className="similar-artists-container">
-      <div className="artist-list">{recommend}</div>
-    </div>
-  );
+  const handleClick = recommend => {
+    console.log(recommend);
+    props.setArtists(recommend)
+  }
+
+    const recommend = !!props.artistRecommendations ? (
+        props.artistRecommendations.artists.map(recommend => {
+          // console.log(recommend);
+            return(
+                <ul className="disp-container">
+                  <li><a onClick={() => handleClick(recommend)}>{recommend.name}</a></li>
+                </ul>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          );
+
+        return (
+                <div className="similar-artists-container">
+                    <div className="artist-list">{recommend}</div>
+                </div>);
 };
+
 
 export default Recommendations;
